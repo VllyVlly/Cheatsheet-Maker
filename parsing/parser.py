7254800ee8b5cbcb11282beb48ce760fa194f103
VllyVlly <virellelly578@gmail.com>
2026-09-14 21:32:11 +0800

    Project init

---
+++ parsing/parser.py
@@ -0,0 +1,16 @@
+from docling.document_converter import DocumentConverter
+from io import BytesIO
+from docling.datamodel.base_models import DocumentStream
+
+
+def parse(converter: DocumentConverter, file_bytes, file_name):
+    if converter is None: 
+        print("Converter not found")
+        return
+    if file_bytes is None or file_name is None: 
+        print("File not found")
+        return
+    buf = BytesIO(file_bytes)
+    source = DocumentStream(name = file_name, stream = buf)
+    return converter.convert(source)
+
